4b9aa019710323f702e0684620d2e9028ebeeb53
Nur almas Norazman <nnorazma@u91z05s01.42kl.edu.my>
2022-04-16 17:38:47 +0800

    changes

--- client.c
+++ client.c
@@ -3,14 +3,41 @@
 #include <stdio.h>
 #include <signal.h>
 
+void	send_binary(int pid, char *str)
+{
+	int i;
+	int j;
+	int	c;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		c = str[i];
+		j = 0;
+		while (j < 8)
+		{
+			if (c % 2 == 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(50);
+			j++;
+			c >>=1;
+		}
+		i ++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	pid_t pid;
 	char *str;
-
-	pid = ft_atoi(argv[1]);
-	str = argv[2];
-	kill(pid, SIGUSR1);
-	printf("pid = %d, str = %s\n", pid, str);
+	if (argc > 1)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		send_binary(pid, str);
+		printf("pid = %d, str = %s\n", pid, str);
+	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -2,16 +2,23 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	sig_handling(int sig)
+void	sig_handling(int sig, siginfo_t *siginfo, void *data)
 {
-	printf("Hi Jordan\n");
+	(void)siginfo;
+	(void)data;
+	printf("Received:");
+	
 }
 
 int main()
 {
 	pid_t pid;
-
-	signal(SIGUSR1, sig_handling);
+	struct sigaction	sa;
+	
+	sa.sa_sigaction = sig_handling;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	pid = getpid();
 	printf("%d\n", pid);
 	while (1)
