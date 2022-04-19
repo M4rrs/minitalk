72f8bc2c457f4ae02b479c898343e54e8f996045
M4rrs <almasnorazman2803@outlook.com>
2022-04-20 02:30:50 +0800

    changed

--- client.c
+++ client.c
@@ -3,48 +3,47 @@
 #include "libft/ft_atoi.c"
 #include "libft/ft_putstr_fd.c"
 
-void	received_message(int sig)
-{
-	static int	received;
-	if (sig == SIGUSR1)
-		received++ ;
-	
-}
-void	send_binary(int pid, char *str)
+void	send_binary(int pid, char c)
 {
 	int i;
-	int j;
-	int	c;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (i < 8)
 	{
-		c = str[i];
-		j = 0;
-		while (j < 8)
-		{
-			if (c % 2 == 0)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			usleep(50);
-			j++;
-			c >>= 1;
-		}
-		i++;
+		if (c >> i & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++ ;
+		usleep(42);
 	}
+}
+
+void	send_message(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i++])
+		send_binary(pid, str[i]);
+	send_binary(pid, 0);
 }
 
 int main(int argc, char **argv)
 {
 	pid_t pid;
 	char *str;
-	if (argc == 3)
+	if (argc < 3)
+	{
+		ft_putstr_fd("No message inputted.\n", 1);
+		exit(EXIT_FAILURE);
+	}
+	else if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
 		str = argv[2];
 		ft_putstr_fd("Sending Signals.\n", 1);
-		send_binary(pid, str);
+		send_message(pid, str);
 		signal(SIGUSR1, received_message);
 		signal(SIGUSR2, received_message);
 	}
--- server.c
+++ server.c
@@ -4,24 +4,53 @@
 #include "libft/ft_putnbr_fd.c"
 #include "libft/ft_putstr_fd.c"
 
-void	receiver(int sig, siginfo_t *info, void *data)
+// void	receiver(int sig, siginfo_t *info, void *data)
+// {
+// 	pid_t	client_pid;
+// 	static int		c;
+// 	static int		i;
+
+// 	(void)data;
+// 	client_pid = info->si_pid;
+// 	if (sig == SIGUSR2)
+// 		c += 128;
+// 	if (i < 7)
+// 		c >>= 1;
+// 	i++ ;
+// 	if (i == 8)
+// 	{
+// 		write(1, &c, 1);
+// 		c = 0;
+// 		i = 0;
+// 	}
+// }
+void	sig_handler(int sig, siginfo_t *info, void *data)
 {
-	pid_t	client_pid;
-	static int		c;
-	static int		i;
+	int	i;
+	int	bit;
+	static int	c;
 
 	(void)data;
-	client_pid = info->si_pid;
 	if (sig == SIGUSR2)
-		c += 128;
+		bit = 0;
+	else
+		bit = 1;
 	if (i < 7)
-		c >>= 1;
-	i++ ;
+	{
+		c += bit;
+		c <<= i;
+	}
+	i ++;
 	if (i == 8)
 	{
 		write(1, &c, 1);
-		c = 0;
+		if (c == '\0')
+		{
+			write(1, '\n', 1);
+			kill(info->si_pid, SIGUSR1);
+		}
 		i = 0;
+		c = 0;
 	}
 }
 
@@ -30,7 +59,7 @@ int main()
 	pid_t pid;
 	struct sigaction	sa;
 	
-	sa.sa_sigaction = receiver;
+	sa.sa_sigaction = sig_handler;
 	sa.sa_flags = SA_SIGINFO;
 	pid = getpid();
 	ft_putstr_fd("Talk to me!\n", 1);
